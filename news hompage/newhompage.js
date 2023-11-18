a2140155832a7bd904dcf80bdfebf5baa97d3fd4
oseiawuahjoshua <104779960+oseiawuahjoshua@users.noreply.github.com>
2023-11-18 19:45:15 +0600

    Add files via upload
    SOLUTION TO NEWSHOMEPAGE CHALLENGE

---
+++ news hompage/newhompage.js
@@ -0,0 +1,12 @@
+const navbar= document.querySelector('.navbar')
+const menuicon  =document.querySelector('.menuicon')
+
+menuicon.addEventListener('click', ()=>{
+    navbar.style.display='block'
+    menuicon.style.display='none'
+    
+})
+
+
+
+
